da8bfab9886b47b3c9d6b9f02d6600ac7e3055de
StherLay <00001103693025sp@al.educacao.sp.gov.br>
2024-08-15 16:14:58 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(700, 800);
+    background("white");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+  
+    // console.log(mouseIsPressed)
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
